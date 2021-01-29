702cbf788e5f6ed5cf917759b51e704b0e647095
dgYoo <db4906@gmail.com>
2021-01-29 10:51:18 +0900

    프레임워크와 라이브러리 스프링 프레임워크의 탄생 배경
    IOC 컨테이너
    
    IOC 컨테이너 생성
     - sts3 플러그인 설치
     - 스프링 설정파일(스프링 빈 생성) 작성
     - scope 테스트(singleton, prototype)
     - collection 객체 생성
     - property-placeholder
     - java config

---
+++ src/main/java/kr/or/ddit/ioc/config/ComponentScanJavaConfig.java
@@ -0,0 +1,12 @@
+package kr.or.ddit.ioc.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan(basePackages = {"kr.or.ddit"})
+@Configuration
+public class ComponentScanJavaConfig {
+
+	//<context:component-scan base-package="kr.or.ddit"></context:component-scan>
+	
+}
