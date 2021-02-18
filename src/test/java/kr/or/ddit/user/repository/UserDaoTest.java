fe38e81d44432a74421289b8c9c4c5431bee3876
dgYoo <db4906@gmail.com>
2021-02-18 11:27:23 +0900

    테스트 db 생성 테스트 환경 설정 파일 분리  (datasource-context_dev.xml, dbInfo_dev.properties) DatabasePopulator를 통한 데이터 초기화 로직 작성

--- src/test/java/kr/or/ddit/user/repository/UserDaoTest.java
+++ src/test/java/kr/or/ddit/user/repository/UserDaoTest.java
@@ -6,8 +6,13 @@ import java.util.Date;
 import java.util.List;
 
 import javax.annotation.Resource;
+import javax.sql.DataSource;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 import kr.or.ddit.common.model.PageVo;
 import kr.or.ddit.test.config.ModelTestConfig;
@@ -19,6 +24,26 @@ public class UserDaoTest extends ModelTestConfig{
 
 	@Resource(name="userDao")
 	private UserDao userDao;
+	
+	@Resource(name="dataSource")
+	private DataSource dataSource;
+	
+	@Before
+	public void setup() {
+		// initData.sql을 실행 : 스프링에서 제공하는 ResourceDatabasePopulator
+		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+		
+		// populator를 통해 실행시킬 sql 파일을 지정
+		populator.addScript(new ClassPathResource("/kr/or/ddit/config/db/initData.sql"));
+		
+		// script 파일을 실행하다 에러가 발생할 경우 
+		// 더이상 진행하지 않고 멈추게 설정
+		populator.setContinueOnError(false);
+		
+		// populator를 실행
+		DatabasePopulatorUtils.execute(populator, dataSource);
+	}
+	
 	
 	@Test
 	public void getUserTest() {
@@ -41,7 +66,7 @@ public class UserDaoTest extends ModelTestConfig{
 		List<UserVo> userList = userDao.selectAllUser();
 
 		/***Then***/
-		assertEquals(20, userList.size());
+		assertEquals(16, userList.size());
 	}
 	
 	// 페이징 처리하여 사용자 정보 조회
@@ -66,7 +91,7 @@ public class UserDaoTest extends ModelTestConfig{
 		int userCnt = userDao.selectAllUserCnt();
 
 		/***Then***/
-		assertEquals(19,userCnt);
+		assertEquals(16, userCnt);
 	}
 	@Test
 	public void modifyUserTest() {
---
+++ src/test/resources/kr/or/ddit/config/db/initData.sql
@@ -0,0 +1,24 @@
+SELECT * FROM PRD_DOES_NOT_EXISTS_TABLE;
+
+DELETE users;
+
+
+Insert into USERS (USERID,USERNM,PASS,REG_DT,ALIAS,ADDR1,ADDR2,ZIPCODE,FILENAME,REALFILENAME) values ('cony','코니','conyPass',to_date('20/10/21','YY/MM/DD'),'토끼',null,null,null,null,null);
+Insert into USERS (USERID,USERNM,PASS,REG_DT,ALIAS,ADDR1,ADDR2,ZIPCODE,FILENAME,REALFILENAME) values ('sally','샐리','sallyPass',to_date('20/10/21','YY/MM/DD'),'병아리',null,null,null,null,null);
+Insert into USERS (USERID,USERNM,PASS,REG_DT,ALIAS,ADDR1,ADDR2,ZIPCODE,FILENAME,REALFILENAME) values ('james','제임스','jamesPass',to_date('20/10/21','YY/MM/DD'),'사람',null,null,null,null,null);
+Insert into USERS (USERID,USERNM,PASS,REG_DT,ALIAS,ADDR1,ADDR2,ZIPCODE,FILENAME,REALFILENAME) values ('moon','문','moonPass',null,'달',null,null,null,null,null);
+Insert into USERS (USERID,USERNM,PASS,REG_DT,ALIAS,ADDR1,ADDR2,ZIPCODE,FILENAME,REALFILENAME) values ('leonard','레너드','leonardPass',to_date('20/10/15','YY/MM/DD'),'개구리',null,null,null,null,null);
+Insert into USERS (USERID,USERNM,PASS,REG_DT,ALIAS,ADDR1,ADDR2,ZIPCODE,FILENAME,REALFILENAME) values ('edward','에드워드','edwardPass',to_date('20/10/15','YY/MM/DD'),'애벌레',null,null,null,null,null);
+Insert into USERS (USERID,USERNM,PASS,REG_DT,ALIAS,ADDR1,ADDR2,ZIPCODE,FILENAME,REALFILENAME) values ('jessica','제시카','jessicaPass',to_date('20/10/15','YY/MM/DD'),'고양이',null,null,null,null,null);
+Insert into USERS (USERID,USERNM,PASS,REG_DT,ALIAS,ADDR1,ADDR2,ZIPCODE,FILENAME,REALFILENAME) values ('boss','보스','bossPass',to_date('20/10/15','YY/MM/DD'),'사람123',null,null,null,null,null);
+Insert into USERS (USERID,USERNM,PASS,REG_DT,ALIAS,ADDR1,ADDR2,ZIPCODE,FILENAME,REALFILENAME) values ('choco','초코','chocoPass',to_date('20/10/15','YY/MM/DD'),'곰2',null,null,null,null,null);
+Insert into USERS (USERID,USERNM,PASS,REG_DT,ALIAS,ADDR1,ADDR2,ZIPCODE,FILENAME,REALFILENAME) values ('pangyo','팡요','pangyoPass',to_date('20/10/15','YY/MM/DD'),'판다',null,null,null,null,null);
+Insert into USERS (USERID,USERNM,PASS,REG_DT,ALIAS,ADDR1,ADDR2,ZIPCODE,FILENAME,REALFILENAME) values ('muzi','무지','muziPass',to_date('20/10/15','YY/MM/DD'),'토끼',null,null,null,null,null);
+Insert into USERS (USERID,USERNM,PASS,REG_DT,ALIAS,ADDR1,ADDR2,ZIPCODE,FILENAME,REALFILENAME) values ('con','콘','conPass',to_date('20/10/15','YY/MM/DD'),'악어',null,null,null,null,null);
+Insert into USERS (USERID,USERNM,PASS,REG_DT,ALIAS,ADDR1,ADDR2,ZIPCODE,FILENAME,REALFILENAME) values ('apeach','어피치','apeachPass',to_date('20/10/15','YY/MM/DD'),'복숭아',null,null,null,null,null);
+Insert into USERS (USERID,USERNM,PASS,REG_DT,ALIAS,ADDR1,ADDR2,ZIPCODE,FILENAME,REALFILENAME) values ('ryan','라이언 ','ryanPass',to_date('20/10/15','YY/MM/DD'),'사자',null,null,null,null,null);
+Insert into USERS (USERID,USERNM,PASS,REG_DT,ALIAS,ADDR1,ADDR2,ZIPCODE,FILENAME,REALFILENAME) values ('brown','브라운','brownPass',to_date('20/10/21','YY/MM/DD'),'곰돌2',null,null,null,null,null);
+Insert into USERS (USERID,USERNM,PASS,REG_DT,ALIAS,ADDR1,ADDR2,ZIPCODE,FILENAME,REALFILENAME) values ('ddit','대덕인재','dditPass',to_date('21/02/02','YY/MM/DD'),'개발원','대전시 중구 중앙로79','4층','34940',null,null);
+
+
+COMMIT;
